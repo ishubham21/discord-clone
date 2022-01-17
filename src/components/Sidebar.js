714dbca06e5886ae04d42b997d91e7f659a5539f
ishubham21 <sg2199203@gmail.com>
2022-01-17 13:36:23 +0530

    adding sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,29 @@
 import style from './Sidebar.module.css'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/AddIcon'
 
 const Sidebar = () => {
-    return(<>
+    return (<>
         <div className={style.sidebar}>
+
             <div className={style.sidebar__top}>
                 <h3>Discord Server</h3>
+                <ExpandMoreIcon />
             </div>
+
+            <div className={style.sidebar__channels}>
+                <div className={style.sidebar__channelsHeader}>
+                    <div className={style.sidebar__header}>
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+                    
+                    <AddIcon onClick={handleAdd} className={style.sidebar__addChannel} />
+                </div>
+            </div>
+
         </div>
     </>)
 }
 
-export default Sidebar
+export default Sidebar  
