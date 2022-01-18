0e3f4a6c504ae762cf2103563d6a52ca28373553
ishubham21 <sg2199203@gmail.com>
2022-01-18 22:00:49 +0530

    building up chatHeader

--- src/components/ChatHeader/ChatHeader.js
+++ src/components/ChatHeader/ChatHeader.js
@@ -1,8 +1,72 @@
 import style from './ChatHeader.module.css'
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import EditLocationRoundedIcon from '@mui/icons-material/EditLocation';
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAlt'
+import SendRoundedIcon from '@mui/icons-material/SendRounded'
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded'
 
 const ChatHeader = () => {
     return ( <>
-        <div className={style.chatHeader}></div>
+        <div className={style.chatHeader}>
+            <div className={style.chatHeader__left}>
+                <h3>
+                    <span className={style.chatHeader__hash}>
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className={style.chatHeader__right}>
+                <NotificationsIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+                <EditLocationRoundedIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+                <PeopleAltRoundedIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+
+                <div className={style.chatHeader__search}>
+                    <input type="text" placeholder='Search'/>
+                    <SearchRoundedIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+                </div>
+
+                <SendRoundedIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+                <HelpRoundedIcon sx={{
+                    p: '10px',
+                    cursor: 'pointer',
+                    "&:hover": {
+                      color: '#fff'
+                    }
+                }}/>
+            </div>
+        </div>
     </> );
 }
  
